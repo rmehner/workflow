3fafdd0827e49a034901bdba3cb26a3a34d589f5
Robin Mehner <robin@coding-robin.de>
2021-04-01 10:39:01 +0200

    Add script to remove android sdk files to start with a clean slate

---
+++ remove-android.sh
@@ -0,0 +1,13 @@
+rm -Rf /Applications/Android\ Studio.app
+rm -Rf ~/Library/Preferences/AndroidStudio*
+rm -Rf ~/Library/Preferences/com.google.android.*
+rm -Rf ~/Library/Preferences/com.android.*
+rm -Rf ~/Library/Application\ Support/AndroidStudio*
+rm -Rf ~/Library/Logs/AndroidStudio*
+rm -Rf ~/Library/Caches/AndroidStudio*
+rm -Rf ~/.AndroidStudio*
+rm -Rf ~/AndroidStudioProjects
+rm -Rf ~/.gradle
+rm -Rf ~/.android
+rm -Rf ~/Library/Android*
+rm -Rf ~/.cordova
